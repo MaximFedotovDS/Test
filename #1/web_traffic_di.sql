09c0a0e59a8c05afb5f97065e8e0d30941255f09
Maxim Fedotov <fedotov.maxim.togliatti@gmail.com>
2022-01-16 16:45:18 +0300

    Create web_traffic_di.sql

---
+++ #1/web_traffic_di.sql
@@ -0,0 +1,38 @@
+CREATE TABLE web_traffic_di (
+member_id Integer,
+channel Varchar(40),
+date Date
+);
+INSERT INTO public.web_traffic_di (member_id,channel,"date") VALUES
+	 (1,'opera','2020-11-25'),
+	 (2,'chrome','2021-11-25'),
+	 (2,'chrome','2021-11-25'),
+	 (3,'opera','2020-11-25'),
+	 (3,'opera','2021-11-25'),
+	 (4,'opera','2020-11-25'),
+	 (5,'opera','2021-11-25'),
+	 (6,'chrome','2020-11-25'),
+	 (7,'chrome','2021-11-25'),
+	 (8,'chrome','2021-11-25');
+INSERT INTO public.web_traffic_di (member_id,channel,"date") VALUES
+	 (1,'opera','2020-11-25'),
+	 (2,'chrome','2021-11-25'),
+	 (2,'chrome','2021-11-25'),
+	 (3,'opera','2020-11-25'),
+	 (3,'opera','2021-11-25'),
+	 (4,'opera','2020-11-25'),
+	 (5,'opera','2021-11-25'),
+	 (6,'chrome','2020-11-25'),
+	 (7,'chrome','2021-11-25'),
+	 (8,'chrome','2021-11-25');
+INSERT INTO public.web_traffic_di (member_id,channel,"date") VALUES
+	 (1,'opera','2020-11-25'),
+	 (2,'chrome','2021-11-25'),
+	 (2,'chrome','2021-11-25'),
+	 (3,'opera','2020-11-25'),
+	 (9,'opera','2021-11-25'),
+	 (10,'opera','2020-11-25'),
+	 (11,'opera','2021-11-25'),
+	 (12,'chrome','2020-11-25'),
+	 (13,'chrome','2021-11-25'),
+	 (14,'chrome','2021-11-25');
