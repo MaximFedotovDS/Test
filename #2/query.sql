2b397b0d80daeb828e37feec1423976179357eb6
Maxim Fedotov <fedotov.maxim.togliatti@gmail.com>
2022-01-16 18:44:04 +0300

    Create query.sql

---
+++ #2/query.sql
@@ -0,0 +1,51 @@
+select
+	distinct channel,
+	member_id
+from
+	web_traffic_di wtd
+where
+	member_id = 
+(
+	select
+		Buyer_id
+	from
+		trade_di
+	where
+		extract(year
+	from
+		date) = 2020
+		and extract(month
+	from
+		date) = 10
+	group by
+		buyer_id
+	order by
+		sum(gmv) desc
+	limit 1
+)
+union all
+select
+	distinct channel,
+	member_id
+from
+	app_traffic_di atd
+where
+	member_id = 
+(
+	select
+		Buyer_id
+	from
+		trade_di
+	where
+		extract(year
+	from
+		date) = 2020
+		and extract(month
+	from
+		date) = 10
+	group by
+		buyer_id
+	order by
+		sum(gmv) desc
+	limit 1
+)
