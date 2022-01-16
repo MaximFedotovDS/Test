cc7b5fd5ddf57c0a88af8e24d20dd0c5f2854f8b
Maxim Fedotov <fedotov.maxim.togliatti@gmail.com>
2022-01-17 00:18:29 +0300

    Create query_YoY.sql

---
+++ #4/query_YoY.sql
@@ -0,0 +1,53 @@
+select
+	a.seller_id,
+	sum_nov / sum_okt * 100 as MoM
+from
+	(
+	select
+		seller_id,
+		sum(gmv) as sum_nov
+	from
+		trade_di td
+	where
+		extract(year
+	from
+		date) = 2020
+		and extract(month
+	from
+		date) = 11
+		and buyer_id in (
+		select
+			member_id
+		from
+			app_traffic_di
+		where
+			Channel = 'ios')
+	group by
+		seller_id) as a
+join 
+(
+	select
+		seller_id,
+		sum(gmv) as sum_okt
+	from
+		trade_di td
+	where
+		extract(year
+	from
+		date) = 2019
+		and extract(month
+	from
+		date) = 11
+		and buyer_id in (
+		select
+			member_id
+		from
+			app_traffic_di
+		where
+			Channel = 'ios')
+	group by
+		seller_id) as b on
+	a.seller_id = b.seller_id
+order by
+	sum_nov / sum_okt * 100 desc
+limit 100;
