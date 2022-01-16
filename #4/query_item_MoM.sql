c47431e4a19353d3aa6b6afcd04c8fb58f74ff15
Maxim Fedotov <fedotov.maxim.togliatti@gmail.com>
2022-01-17 00:57:52 +0300

    Create query_item_MoM.sql

---
+++ #4/query_item_MoM.sql
@@ -0,0 +1,57 @@
+select
+	Item_name,
+	sum_nov,
+	sum_okt,
+	sum_nov / sum_okt * 100 as MoM
+from
+	(
+	select
+		Item_id,
+		sum(gmv) as sum_nov
+	from
+		trade_di td
+	where
+		extract(year
+	from
+		date) = 2020
+		and extract(month
+	from
+		date) = 11
+		and buyer_id in (
+		select
+			member_id
+		from
+			app_traffic_di
+		where
+			Channel = 'ios')
+	group by
+		seller_id) as a
+join 
+(
+	select
+		Item_id,
+		sum(gmv) as sum_okt
+	from
+		trade_di td
+	where
+		extract(year
+	from
+		date) = 2020
+		and extract(month
+	from
+		date) = 10
+		and buyer_id in (
+		select
+			member_id
+		from
+			app_traffic_di
+		where
+			Channel = 'ios')
+	group by
+		seller_id) as b on
+	a.seller_id = b.seller_id
+join dim_item as di on
+	di.Item_id = a.Item_id
+order by
+	sum_nov / okt * 100 desc
+limit 100;
