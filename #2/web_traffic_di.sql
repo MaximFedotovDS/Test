2bb65aaba9491164c506966a8d4e441e9615661f
Maxim Fedotov <fedotov.maxim.togliatti@gmail.com>
2022-01-16 18:26:15 +0300

    Create web_traffic_di.sql

---
+++ #2/web_traffic_di.sql
@@ -0,0 +1,14 @@
+create table web_traffic_di (
+Member_id integer,
+Channel varchar(40),
+Date date
+);
+
+insert into app_traffic_di values
+(7, 'chrome', '25-11-2020'),
+(8, 'chrome', '25-11-2020'),
+(9, 'edge', '25-11-2021'),
+(10, 'opera', '25-11-2020'),
+(10, 'opera', '26-11-2020'),
+(11, 'chrome', '25-11-2020'),
+(12, 'chrome', '25-11-2020');
