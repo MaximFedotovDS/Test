1aed3e7f3e61b33e9a76b37aa194c506bb44e598
Maxim Fedotov <fedotov.maxim.togliatti@gmail.com>
2022-01-16 18:25:59 +0300

    Create app_traffic_di.sql

---
+++ #2/app_traffic_di.sql
@@ -0,0 +1,14 @@
+create table app_traffic_di (
+Member_id integer,
+Channel varchar(40),
+Date date
+);
+
+insert into app_traffic_di values
+(1, 'ios', '25-11-2020'),
+(2, 'ios', '25-11-2020'),
+(3, 'ios', '25-11-2021'),
+(4, 'android', '25-11-2020'),
+(4, 'android', '26-11-2020'),
+(5, 'ios', '25-11-2020'),
+(6, 'android', '25-11-2020');
