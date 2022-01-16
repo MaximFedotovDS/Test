e43803fe791f9e770a2c0d1ef298cc403e8c9987
Maxim Fedotov <fedotov.maxim.togliatti@gmail.com>
2022-01-16 16:40:05 +0300

    Create query.sql

---
+++ #1/query.sql
@@ -0,0 +1,32 @@
+select
+		channel,
+		count(distinct member_id)
+from
+		web_traffic_di
+where
+		extract (year
+from
+		Date) = 2020
+	and extract(month
+from
+		Date) = 11
+group by
+		channel
+union all
+	select
+		channel,
+		count(distinct member_id)
+from
+		app_traffic_di
+where
+		extract (year
+from
+		Date) = 2020
+	and extract(month
+from
+		Date) = 11
+group by
+		channel
+order by
+		count desc
+limit 1;
